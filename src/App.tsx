324417b679c5fa41073d5bdb959af1604513605d
Sid Rao <siddhartha.s.rao@gmail.com>
2021-02-14 15:23:40 -0800

    continuing to fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We now have auth!
+          We now have auth with a role!
         </p>
         <a
           className="App-link"
