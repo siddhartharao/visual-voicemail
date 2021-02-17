b324e4766c530a808dc90f89615d293960c7c72d
Sid Rao <siddhartha.s.rao@gmail.com>
2021-02-16 17:29:09 -0800

    Added storage and SMA function

---
+++ amplify/backend/function/voicemailsma/src/index.js
@@ -0,0 +1,27 @@
+/* Amplify Params - DO NOT EDIT
+	API_VISUALVOICEMAIL_GRAPHQLAPIENDPOINTOUTPUT
+	API_VISUALVOICEMAIL_GRAPHQLAPIIDOUTPUT
+	API_VISUALVOICEMAIL_GRAPHQLAPIKEYOUTPUT
+	API_VISUALVOICEMAIL_MAILBOXTABLE_ARN
+	API_VISUALVOICEMAIL_MAILBOXTABLE_NAME
+	API_VISUALVOICEMAIL_VOICEMAILTABLE_ARN
+	API_VISUALVOICEMAIL_VOICEMAILTABLE_NAME
+	AUTH_VISUALVOICEMAIL8AEA0F86_USERPOOLID
+	ENV
+	REGION
+	STORAGE_VISUALVOICEMAIL167E06E1_BUCKETNAME
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
